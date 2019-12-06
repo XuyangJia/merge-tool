773ba57d928a0527124331435fc74243447bc424
Xuyang Jia <jiaxuyang@outlook.com>
2019-12-06 18:41:20 +0800

    删除测试文件

--- src/jia.js
+++
@@ -1,8 +0,0 @@
-function checkout (zones) {
-  const countrys = zones.map(item => {
-    return [`${item}_0`, `${item}_1`, `${item}_2`]
-  })
-  console.log(countrys)
-  // console.log([ [ 'h1_0', 'h1_1', 'h1_2' ], [ 'h2_0', 'h2_1', 'h2_2' ] ].flat())
-}
-checkout(['h1', 'h2'])
